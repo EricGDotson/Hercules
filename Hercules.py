749d7e4c8991d68c1a008b7b797c19667cd13f84
Eric Dotson <eric.dotson@integer-tech.com>
2024-01-19 14:56:21 -0500

    Importing json and working server need to make the work togerther.

--- Hercules.py
+++ Hercules.py
@@ -1,6 +1,14 @@
 # Opening config Java Script Object Notation (json) file
 import json
 import socket
+import time
+from http.server import HTTPServer, BaseHTTPRequestHandler
+
+# TODO: put config in gitignore
+
+host = "192.168.10.121"
+port = 1002
+date = time.strftime("%m/%d/%Y")
 
 try:
     with open("config.json", "r") as f:
@@ -8,33 +16,41 @@ try:
         print(data)
 except FileNotFoundError:
     print("Error: File not found.")
-    exit
-
-'''
-create socket
-AF_INET - address family, where host is a string representing either a hostname in internet domain notation like (default)
-SOCK_STREAM - Defualt socket type streaming 
-'''
-socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-
-# Defines host
-host = '192.168.10.100'
-port = 1001
-# do I need port?
-
-try:
-    socket.bind((host, port))
-except:
-    print("Can not bind to port")
     exit()
 
+'''
+This class allows us to create http server and get a response from it
+BasrHTTPRequestHandler - used to handle the HHTP request that arrive at the server, 
+which pre-opens sockets 
+'''
+
+
+class HTTPResponse(BaseHTTPRequestHandler):
+
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+
+        self.wfile.write(
+            bytes("HELLO name, today is ".encode() + date.encode()))
+
+
+Server = HTTPServer((host, port), HTTPResponse)
+print("Serever now running")
+Server.serve_forever()
+Server.server_close()
+print("Server stop")
 
 '''
-    Bind to {port}
-    If fail exit
+os.walk Append dir to a list
+walking through home append names to list then make if statement
 
-    Give HTTP response of "Hello {name} today is {day}"
+if name is in list find that name in json 
+grab that name and port number for port and print 
 
 
-exit
+why is config in gitignore
+for each name
+
 '''
